84ab3d719984de46d5106d27987055e7001dd45b
Iswan Jumat <iswan@thinkcube.com>
2015-11-15 00:11:55 +0530

    added button component styles

---
+++ src/styles/button.js
@@ -0,0 +1,75 @@
+import Color from 'color';
+import helpers from './util/ButtonHelpers';
+import $ from './util/variables';
+
+export default {
+  base: {
+    backgroundColor: $.def,
+    borderStyle: 'solid',
+    borderWidth: '1px',
+  	borderColor: Color($.def).darken(0.1).hexString(),
+  	display: 'inline-block',
+  	marginBottom: 0, // For input.btn
+  	marginRight: '.3em',
+    outline: 'none',
+  	fontWeight: 400,
+  	textAlign: 'center',
+  	verticalAlign: 'middle',
+  	touchAction: 'manipulation',
+  	cursor: 'pointer',
+  	backgroundImage: 'none',
+  	textDecoration: 'none',
+  	whiteSpace: 'nowrap',
+    borderRadius: $.bordeRadiusBase,
+  },
+  normalSize: {
+    padding: `${$.button.btnPaddingVertical} ${$.button.btnPaddingHorizontal}`,
+    fontSize: '.9em',
+    lineHeight: 1.444,
+    borderRadius: $.bordeRadiusBase,
+  },
+  small: {
+    padding: `${$.button.btnPaddingSmVertical} ${$.button.btnPaddingSmHorizontal}`,
+    fontSize: '.75em',
+    lineHeight: 1.444,
+    borderRadius: $.bordeRadiusBase,
+  },
+  large: {
+    padding: `${$.button.btnPaddingLgVertical} ${$.button.btnPaddingLgHorizontal}`,
+    fontSize: '1.4em',
+    fontWeight: '400',
+    lineHeight: 1.444,
+    borderRadius: $.bordeRadiusBase,
+  },
+  normal: helpers.buttonVariant($.textNormal, $.def, $.def),
+  normalClicked: helpers.buttonVariantClicked($.textNormal, $.def, $.def),
+  normalHovered: helpers.buttonVariantHovered($.textNormal, $.def, $.def),
+
+  primary: helpers.buttonVariant($.textWhite, $.primary, $.primary),
+  primaryClicked: helpers.buttonVariantClicked($.textWhite, $.primary, $.primary),
+  primaryHovered: helpers.buttonVariantHovered($.textWhite, $.primary, $.primary),
+
+  danger: helpers.buttonVariant($.textWhite, $.danger, $.danger),
+  dangerClicked: helpers.buttonVariantClicked($.textWhite, $.danger, $.danger),
+  dangerHovered: helpers.buttonVariantHovered($.textWhite, $.danger, $.danger),
+
+  secondary: helpers.buttonVariant($.textWhite, $.secondary, $.secondary),
+  secondaryClicked: helpers.buttonVariantClicked($.textWhite, $.secondary, $.secondary),
+  secondaryHovered: helpers.buttonVariantHovered($.textWhite, $.secondary, $.secondary),
+
+  normal_o: helpers.buttonVariant($.textNormal, 'transparent', $.def),
+  normal_oClicked: helpers.buttonVariantClicked(Color($.textNormal).darken(0.1), 'transparent', $.def),
+  normal_oHovered: helpers.buttonVariantHovered( Color($.textNormal).darken(0.15).hexString(), 'transparent', $.def),
+
+  primary_o: helpers.buttonVariant($.textNormal, 'transparent', $.primary),
+  primary_oClicked: helpers.buttonVariantClicked(Color($.primary).darken(0.1), 'transparent', $.primary),
+  primary_oHovered: helpers.buttonVariantHovered( Color($.primary).darken(0.15).hexString(), 'transparent', $.primary),
+
+  danger_o: helpers.buttonVariant($.textNormal, 'transparent', $.danger),
+  danger_oClicked: helpers.buttonVariantClicked(Color($.danger).darken(0.1), 'transparent', $.danger),
+  danger_oHovered: helpers.buttonVariantHovered( Color($.danger).darken(0.15).hexString(), 'transparent', $.danger),
+
+  secondary_o: helpers.buttonVariant($.textNormal, 'transparent', $.secondary),
+  secondary_oClicked: helpers.buttonVariantClicked(Color($.secondary).darken(0.1), 'transparent', $.secondary),
+  secondary_oHovered: helpers.buttonVariantHovered( Color($.secondary).darken(0.15).hexString(), 'transparent', $.secondary),
+};
---
+++ src/styles/util/variables.js
@@ -0,0 +1,29 @@
+import Color from 'color';
+export default {
+  bodyBg: '#fff',
+  primary: '#1f90ff',
+  secondary: '#fcb117',
+  success: '#14892c',
+  warning: '#f79232',
+  danger: '#d04437',
+  def: '#eeeeee',
+  //borders
+  bordeRadiusBase: '2px',
+  borderRadiusSmall: '1px',
+  //font colors
+  textNormal: '#333',
+  textSecoandary: '#666',
+  textSmall: '#888',
+  textWhite: '#fff',
+  button: {
+      // Buttons padding
+      btnPaddingVertical: '.5em',
+      btnPaddingHorizontal:	'1.5em',
+      // Button small padding
+      btnPaddingSmVertical: '.4em',
+      btnPaddingSmHorizontal: '.8em',
+      // Button extra large padding
+      btnPaddingLgVertical: '.6em',
+      btnPaddingLgHorizontal: '2.9em'
+  }
+}
